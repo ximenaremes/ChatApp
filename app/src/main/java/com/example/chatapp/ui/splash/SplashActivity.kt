5cf18708d3399538d91728c1d75a8a06b36f2d59
Ximena Remeș <ximenaremes@gmail.com>
2023-01-03 13:58:10 +0200

    First commit

---
+++ app/src/main/java/com/example/chatapp/ui/splash/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.example.chatapp.ui.splash
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.chatapp.ui.main.MainActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+
+    }
+}
